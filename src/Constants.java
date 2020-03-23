c22b47d8a312274caad57ca83e9cab656df8c47a
AlexandreLadriere <45234112+alexandreladriere@users.noreply.github.com>
2020-03-23 17:13:50 +0100

    add: Adding constants file
    Constants.java contains all the characters that will be used by the password generator.
    
    Signed-off-by: AlexandreLadriere <alexandre.ladriere77@gmail.com>

---
+++ src/Constants.java
@@ -0,0 +1,10 @@
+public final class Constants {
+    public Constants() {
+        // restrict instantiation
+    }
+
+    public static final String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    public static final String numbers = "0123456789";
+    public static final String specialCharacters = "@%+\\/'!#$^?:.(){}[]~-_`"; // see: https://docs.oracle.com/cd/E11223_01/doc.910/e11197/app_special_char.htm#MCMAD416
+}
